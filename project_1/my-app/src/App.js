0b00c27ff4f34970db1c3584d065796fb3a0782c
karamalesa1980 <karamalesa@gmail.com>
2019-08-07 23:12:34 +0300

    lesson 7 finished

--- project_1/my-app/src/App.js
+++ project_1/my-app/src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+
+
+
+const App = () => {
   return (
+    <div>
+    <div><Header /></div>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Project_1 <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a>
+        Home
+      </a>
+     <ul>
+      <li>
+        Hello
+      </li>
+      <li>
+        World
+      </li>
+     </ul>
     </div>
+    </div>
+     
+  );
+}
+
+
+const Header = () => {
+  return (
+    
+    <div>
+      <a>
+        Contacts
+      </a>
+
+    </div>  
+      
+    
+     
   );
 }
 
 export default App;
+
+
+
+
