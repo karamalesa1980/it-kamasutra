fadaabd5d5e5d52ed1313a58214d042593f6a24e
karamalesa1980 <karamalesa@gmail.com>
2019-08-08 14:20:29 +0300

    lesson 11 finished

--- project_1/my-app/src/App.js
+++ project_1/my-app/src/App.js
@@ -1,30 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
-import Header from './Header.js';
-import Technologies from './Technologies';
+
 
 
 
 const App = () => {
   return (
-    <div>
-      <div>
-        <Header />
-        <Technologies />
-      </div>
-      <div className="App">
-        <a>
-          Home
-      </a>
-        <ul>
-          <li>
-            Hello
-      </li>
-          <li>
-            World
-      </li>
-        </ul>
-      </div>
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='https://st4.depositphotos.com/18657574/21182/v/1600/depositphotos_211824316-stock-illustration-drop-vector-icon-isolated-on.jpg'></img>
+      </header>
+      <nav className='nav'>
+        <div>
+          <a href='#'>Messages</a>
+        </div>
+        <div>
+          <a href='#'>Profile</a>
+        </div>
+        <div>
+          <a href='#'>News</a>
+        </div>
+        <div>
+          <a href='#'>Muzic</a>
+        </div>
+      </nav>
+      <div className='contant'>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkz6P0VSCzO5Vw5uc6MKgcwS0nRUgzOQMbyfzJk34KU2VQ9aDw'/>
+        </div>
+        <div className='footer'>
+        Footer
+        </div>
+
     </div>
 
   );
