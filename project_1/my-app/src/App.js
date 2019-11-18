3973c96d02747c756de405cbb68426adf09e1e0d
karamalesa1980 <karamalesa@gmail.com>
2019-11-18 15:40:36 +0200

    add folders for everyone components

--- project_1/my-app/src/App.js
+++ project_1/my-app/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Profile from './components/Profile';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import Profile from './components/Profile/Profile';
 
 
 
