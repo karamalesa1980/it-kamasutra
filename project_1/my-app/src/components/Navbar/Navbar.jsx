53c0242f1f3be05b5406c309d9ecf2c3c8791f35
karamalesa1980 <karamalesa@gmail.com>
2019-11-19 16:51:51 +0200

    add react-router-dom -save

--- project_1/my-app/src/components/Navbar/Navbar.jsx
+++ project_1/my-app/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import classes from './Navbar.module.css';
-console.log(classes);
 
 const Navbar = () => {
   return <nav className={classes.nav}>
   <div className={classes.item}>
-    <a>Messages</a>
+    <a href="/dialogs">Messages</a>
   </div>
   <div className={classes.item}>
-    <a>Profile</a>
+    <a href="/profile">Profile</a>
   </div>
   <div className={`${classes.item} ${classes.active}`}>
     <a>News</a>
--- project_1/my-app/src/components/Profile/MyPosts/MyPosts.jsx
+++ project_1/my-app/src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from './MyPosts.module.css';
 import Post from './Post/Post';
 
-const MyPosts = () => {
+const MyPosts = (props) => {
   return (
     <div>
     
