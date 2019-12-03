cc4dc1fa5a4e1ccd16bc76c0a0c43757c40f326f
karamalesa1980 <karamalesa@gmail.com>
2019-12-03 22:39:58 +0200

    add value (posts:, dialogs:, messages:)

---
+++ project_1/my-app/src/redux/state.js
@@ -0,0 +1,24 @@
+let state = {
+  posts: [
+    {id: 1, message: 'Hi Yuriy', like: '15'},
+    {id: 2, message: 'Hi Anton', like: '25'},
+  ],
+  
+  dialogs: [
+    {id: 1, name: 'Yuriy', avatar: 'avatar.jpg'},
+    {id: 2, name: 'Anton', avatar: 'avatar.jpg'},
+    {id: 3, name: 'Marina', avatar: 'avatar.jpg'},
+    {id: 4, name: 'Irina', avatar: 'avatar.jpg'},
+    {id: 5, name: 'Igor', avatar: 'avatar.jpg'},
+  ],
+  
+  messages: [
+    {id: 1, message: 'Hi Yuriy'},
+    {id: 2, message: 'Hi Anton'},
+    {id: 3, message: 'Hi Marina'},
+    {id: 4, message: 'Hi Irina'},
+    {id: 5, message: 'Hi Igor!'},
+  ]
+}
+
+export default state;
