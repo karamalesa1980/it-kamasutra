df851609fbc72eee61ab694c8852fc17c47a293d
karamalesa1980 <karamalesa@gmail.com>
2019-12-08 22:32:58 +0200

    add and include  let store lesson 37 finished

--- project_1/my-app/src/index.js
+++ project_1/my-app/src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import state, {suscribe} from './redux/state';
-import {addpost, addmessage, updatenewpostText, updatenewmessageText} from './redux/state'
+import store from './redux/state';
 import { BrowserRouter } from 'react-router-dom';
 
 
@@ -12,18 +11,18 @@ let rerenderEntireTree = () => {
   ReactDOM.render(
     <BrowserRouter>
       <App
-      appstate={state}
-      addpost={addpost}
-      addmessage={addmessage}
-      updatenewpostText={updatenewpostText}
-      updatenewmessageText={updatenewmessageText}
+      appstate={store.getState()}
+      addpost={store.addpost.bind(store)}
+      addmessage={store.addmessage.bind(store)}
+      updatenewpostText={store.updatenewpostText.bind(store)}
+      updatenewmessageText={store.updatenewmessageText.bind(store)}
       />
     </BrowserRouter>, document.getElementById('root'));
 };
 
-rerenderEntireTree(state, addpost, addmessage, updatenewpostText, updatenewmessageText);
+rerenderEntireTree(store);
 
-suscribe(rerenderEntireTree);
+store.suscribe(rerenderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- project_1/my-app/src/redux/state.js
+++ project_1/my-app/src/redux/state.js
@@ -1,77 +1,75 @@
-
-let rerenderEntireTree = () => {
-};
-
-export const suscribe = (observer) => {
-  rerenderEntireTree = observer;
-};
-
-
-let state = {
-  posts: [
-    {id: 1, message: 'Hi Yuriy', like: '15'},
-    {id: 2, message: 'Hi Anton', like: '25'},
-    {id: 3, message: 'Hi Tema', like: '5'}
-  ],
+let store = {
+  _state: {
+    posts: [
+      {id: 1, message: 'Hi Yuriy', like: '15'},
+      {id: 2, message: 'Hi Anton', like: '25'},
+      {id: 3, message: 'Hi Tema', like: '5'}
+    ],
+    
+    newpostText: 'karamalesa!!!',
+    newmessageText: 'message!!!',
   
-  newpostText: 'karamalesa!!!',
-  newmessageText: 'message!!!',
+    dialogs: [
+      {id: 1, name: 'Yuriy', avatar: 'avatar.jpg'},
+      {id: 2, name: 'Anton', avatar: 'avatar.jpg'},
+      {id: 3, name: 'Marina', avatar: 'avatar.jpg'},
+      {id: 4, name: 'Irina', avatar: 'avatar.jpg'},
+      {id: 5, name: 'Igor', avatar: 'avatar.jpg'},
+    ],
+    
+    messages: [
+      {id: 1, message: 'Hi Yuriy'},
+      {id: 2, message: 'Hi Anton'},
+      {id: 3, message: 'Hi Marina'},
+      {id: 4, message: 'Hi Irina'},
+      {id: 5, message: 'Hi Igor!'},
+    ]
+  },
+  getState() {
+    return this._state;
+  },
 
-  dialogs: [
-    {id: 1, name: 'Yuriy', avatar: 'avatar.jpg'},
-    {id: 2, name: 'Anton', avatar: 'avatar.jpg'},
-    {id: 3, name: 'Marina', avatar: 'avatar.jpg'},
-    {id: 4, name: 'Irina', avatar: 'avatar.jpg'},
-    {id: 5, name: 'Igor', avatar: 'avatar.jpg'},
-  ],
-  
-  messages: [
-    {id: 1, message: 'Hi Yuriy'},
-    {id: 2, message: 'Hi Anton'},
-    {id: 3, message: 'Hi Marina'},
-    {id: 4, message: 'Hi Irina'},
-    {id: 5, message: 'Hi Igor!'},
-  ]
-}
+  callSuscriber() {
+  },
 
+  suscribe(observer) {
+    this.callSuscriber = observer;
+  },
 
-export let addmessage = () => {
-  
-  let newmessage = {
-    id: 4,
-    message: state.newmessageText
-  };
-  state.messages.push(newmessage);
-  state.newmessageText = '';
-  rerenderEntireTree();
-};
+  addmessage() {  
+    let newmessage = {
+      id: 4,
+      message: this._state.newmessageText
+    };
+    this._state.messages.push(newmessage);
+    this._state.newmessageText = '';
+    this.callSuscriber();
+  },
 
-export let updatenewmessageText = (newtext) => {
-  
-  state.newmessageText = newtext;
-  rerenderEntireTree();
-};
+  updatenewmessageText(newtext) {  
+    this._state.newmessageText = newtext;
+    this.callSuscriber();
+  },
 
+  addpost() { 
+    let newpost = {
+      id: 4,
+      message: this._state.newpostText,
+      like: 3
+    };
+    this._state.posts.push(newpost);
+    this._state.newpostText = '';
+    this.callSuscriber();
+  },
 
+  updatenewpostText(newtext) {  
+    this._state.newpostText = newtext;
+    this.callSuscriber();
+  }
 
-export let addpost = () => {
-  
-  let newpost = {
-    id: 4,
-    message: state.newpostText,
-    like: 3
-  };
-  state.posts.push(newpost);
-  state.newpostText = '';
-  rerenderEntireTree();
-};
-
-export let updatenewpostText = (newtext) => {
-  
-  state.newpostText = newtext;
-  rerenderEntireTree();
 };
 
 
 
-export default state;
+export default store;
+window.store = store;
